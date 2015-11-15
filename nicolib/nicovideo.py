77e7645ffb3a4342a33e3a19be98e1dcff83fb63
Tomohiro Nakamura <quickness.net@gmail.com>
2015-11-15 22:39:19 +0900

    wip

--- nicolib/nicovideo.py
+++ nicolib/nicovideo.py
@@ -9,16 +9,18 @@ class NMovie(object):
         self.movie_id = movie_id
         self._info = {}
 
-    def refresh_info(self):
-        res = get_getthumbinfo(self.movie_id)
-        self._info = {}
+    @staticmethod
+    def _get_info(movie_id):
+        information = {}
+        res = get_getthumbinfo(movie_id)
         for elem in res['children'][0]['children']:
             tag = elem.pop('tag')
-            self._info[tag] = elem
+            information[tag] = elem
+        return information
 
     def get_info(self):
         if not self._info:
-            self.refresh_info()
+            self._info = self._get_info(self.movie_id)
         return self._info
 
 
@@ -34,23 +36,24 @@ class NUser(object):
 
 
 def get_getthumbinfo(movie_id):
-    base_url = 'http://ext.nicovideo.jp/api/'
-    ext_url = 'getthumbinfo/{movie_id:s}'.format(movie_id=movie_id)
-    target_url = base_url + ext_url
+    base_url = ('http://ext.nicovideo.jp/api/'
+                'getthumbinfo/{movie_id:s}')
 
     try:
-        _response = urlopen(target_url)
+        _response = urlopen(
+            base_url
+            .format(movie_id=movie_id))
         return xml2dict(_response.read())
     finally:
         _response.close()
 
 
 def get_thumb(movie_id):
-    base_url = 'http://ext.nicovideo.jp/api/{movie_id:s}'
+    base_url = 'http://ext.nicovideo.jp/thumb/{movie_id:s}'
 
     try:
         _response = urlopen(base_url.format(movie_id=movie_id))
-        return _response.read()
+        return _response.read().decode('utf-8')
     finally:
         _response.close()
 
@@ -81,20 +84,26 @@ def get_msg(movie_id):
 
 
 def get_mylist(mylist_id, rss_type='atom'):
-    base_url = 'http://www.nicovideo.jp/mylist/{mylist_id:s}?rss={rss_type:s}'
+    base_url = ('http://www.nicovideo.jp/mylist/'
+                '{mylist_id:s}?rss={rss_type:s}')
+
     try:
         _response = urlopen(
-            base_url.format(mylist_id=mylist_id, rss_type=rss_type))
+            base_url
+            .format(mylist_id=mylist_id, rss_type=rss_type))
         return xml2dict(_response.read())
     finally:
         _response.close()
 
 
 def get_uploaded(user_id, rss_type='atom'):
-    base_url = 'http://www.nicovideo.jp/user/{user_id:s}/video?rss={rss_type:s}'
+    base_url = ('http://www.nicovideo.jp/user/'
+                '{user_id:s}/video?rss={rss_type:s}')
+
     try:
         _response = urlopen(
-            base_url.format(user_id=user_id, rss_type=rss_type))
+            base_url
+            .format(user_id=user_id, rss_type=rss_type))
         return xml2dict(_response.read())
     finally:
         _response.close()
